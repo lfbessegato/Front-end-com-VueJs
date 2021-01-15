f90552b09e8d0c53be50c547d94511f81b460179
Lfbessegato <Lfrbessegato@gmail.com>
2021-01-15 20:26:50 -0300

    Capitulo 06 - Componentes Juntos

---
+++ 06-componentes-juntos/src/LVBusca.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+      <h3>Buscando rivais</h3>
+      <label>Time:</label>
+      <input type="text" v-model="busca">
+      <p>Rival: {{ rival(busca) }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'lv-busca',
+    data() {
+        return {
+            busca: ''
+        }
+    },
+    methods: {
+        rival(time) {
+            switch( time.toLowerCase() ) {
+                case 'corinthians':
+                    return 'palmeiras'
+                case 'santos':
+                    return 'são paulo'
+                case 'flamengo':
+                    return 'vasco'
+                case 'cruzeiro':
+                    return 'em minas é marmelada'
+                default:
+                    return 'não encontrado'
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
